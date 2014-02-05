a76314759b5d0d8f3b783060b7353a3bbf120dfc
Max <maxerich@umich.edu>
2014-02-05 14:53:01 -0700

    final push

--- main.js
+++ main.js
@@ -1,26 +1,19 @@
 // Part 1
 
-var Fooditem = function(name, calories) {
+var Fooditem = function(name, calories, isVegan, isGF, isCF) {
 	this.name = name;
 	this.calories = calories;
-	this.vegan = false;
-	this.glutenFree = false;
-	this.citrusFree = false; 
+	this.vegan = isVegan;
+	this.glutenFree = isGF;
+	this.citrusFree = isCF; 
 }
 
 Fooditem.prototype.toString = function() {
-	return this.name   + " contains " + this.calories + " calories . Vegan: " + this.vegan + ", Gluten Free: " + this.glutenFree + ", Citrus Free: " + this.citrusFree + "." ;
+	var vegan = this.vegan ? "- vegan " : "";
+	var gf = this.glutenFree ? "- gluten free " : "";
+	var cf = this.citrusFree ? "- citrus free " : "";
+	return this.name  + " - " + this.calories + "cal. " + vegan + gf + cf + "\n" ;
 }
-
-var pizza = new Fooditem("pizza", 700)
-
-var turduken = new Fooditem("turduken", 2000)
-
-var broccoli = new Fooditem("broccoli", 40)
-
-
-
-
 
 
 // Part 2
@@ -36,8 +29,8 @@ Drink.prototype.toString = function () {
 	return this.name + "\n" + 
 	this.description + "\n" + 
 	"$" + this.price + "\n" + 
-	"List of ingredients: " + 
-	this.ingredients
+	"It has: " + 
+	this.ingredients.join("");
 }
 
 
@@ -45,55 +38,135 @@ var Plate = function(name, description, price, arrFoodItem) {
 	this.name = name;
 	this.description = description;
 	this.price = price;
-	this.ingredients = arrFoodItem
-}
+	this.ingredients = arrFoodItem;
+};
 
 Plate.prototype.toString = function() {
 	return this.name + "\n" + 
 	this.description + "\n" + 
 	"$" + this.price + "\n" + 
-	"List of ingredients: " + 
-	this.ingredients + " \n"
-}
+	"It has: " + 
+	this.ingredients.join("") + " \n";
+};
+Plate.prototype.isVegan = function(aryFoodItem){
+	var veganCheck = true;
+	for (var i=0; i<this.ingredients.length;i++){
+		if (this.ingredients[i].vegan){	
+		
+		} 
+		else {
+			veganCheck = false;
+		}
+	}
+	return veganCheck;
+};
+Plate.prototype.isGlutenFree = function(aryFoodItem){
+	var gfCheck = true;
+	for (var i=0; i<this.ingredients.length;i++){
+		if (this.ingredients[i].glutenFree){	
+		
+		} 
+		else {
+			gfCheck = false;
+		}
+	}
+	return gfCheck;
+};
+Plate.prototype.isCitrusFree = function(aryFoodItem){
+	var citrusCheck = true;
+	for (var i=0; i<this.ingredients.length;i++){
+		if (this.ingredients[i].citrusFree){	
+		
+		} 
+		else {
+			citrusCheck = false;
+		}
+	}
+	return citrusCheck;
+};
 
 var Order = function(arrPlates) {
 	this.plates = arrPlates;
-// }
-// Order.prototype.arrOfPlates = function(plates)
-}
+};
 
 Order.prototype.toString =  function() {
 	var newarr = map(this.plates, function(item) {
 		return  item.toString()
 	});
 	return newarr.join("")
-}
+};
 
 
 var Menu = function(arrPlates) {
 	this.plates = arrPlates;
-}
+};
+
+Menu.prototype.toString =  function() {
+	var newarr = map(this.plates, function(item) {
+		return  item.toString()
+	});
+	return newarr.join("")
+};
 
 var Restaurant = function(name, description, menu) {
 	this.name = name;
 	this.description = description;
 	this.menu = menu;
+};
+Restaurant.prototype.toString = function(){
+	return this.name + "\n" +
+		this.description + "\n" +
+		this.menu;
 }
 
 var Customer = function(dietaryPreference) {
 	this.dietaryPreference = dietaryPreference;
-}
+};
+Customer.prototype.toString = function(){
+	return this.dietaryPreference;
+};
+
+var pizza = new Fooditem("pizza", 700, false, false, true);
+var turduken = new Fooditem("turduken", 2000, false, true, true);
+var broccoli = new Fooditem("broccoli", 40, true, true, true);
+var fries = new Fooditem("fries", 300, true, false, true);
+var water = new Fooditem("water", 0, true, true, true);
+var sierraMist = new Fooditem("sierra mist", 90, true, true, false);
+var grillCheese = new Fooditem("grilled cheese", 500, false, false, true);
+var macNCheese = new Fooditem("mac and cheese", 800, false, false, true);
+var hamburger = new Fooditem("hamburger", 1200, false, false, true);
+
+var beer = new Fooditem("beer", 180, true, false, true);
+var tortilla = new Fooditem("tortilla", 100, true, false, true);
+var burritoFilling = new Fooditem("burrito filling", 600, false, false, false);
+var avocado = new Fooditem("avocado", 250, true, true, true);
+var salsa = new Fooditem("salsa", 150, true, true, false);
+var tequila = new Fooditem("tequila", 69, true, true, true);
+var margaritaMix = new Fooditem("margarita mix", 200, true, true, false);
+var spanishRice = new Fooditem("spanish rice", 100, false, true, true);
+var chips = new Fooditem("tortilla chips", 200, true, false, false);
 
 
-var Plate1 = new Plate("pizza", "lots of cheese", 8, ["cheese", "tomatoe" , "basil"])
-var Plate2 = new Plate("mac and cheese", "more cheese", 6, ["cheese", "macaroni", "butter"])
-var Plate3 = new Plate("hamburger", "juicy and nice", 10, ["beef", "lettuce", "cheddar"])
+var burritoPlate = new Plate("burrito plate", "burrito with rice and a beer", 12, [tortilla,burritoFilling,salsa,beer]);
+var guacamolePlate = new Plate("guacamole plate", "guacamole with chips", 8, [chips, avocado, salsa]);
+var MargaritaDrink = new Drink("margarita", "frozen margarita drink", 6, [tequila, margaritaMix]);
 
-var arrOfPlates = [Plate1, Plate2, Plate3]
+var restaurantMenu = new Menu([burritoPlate, guacamolePlate, MargaritaDrink]);
 
-var newOrder = new Order(arrOfPlates)
+var itDiner = new Restaurant("IT Diner", "Coolest Diner in the World", restaurantMenu);
 
-newOrder.toString()
+
+itDiner.toString();
+
+var Plate1 = new Plate("pizza plate", "lots of cheese", 8, [pizza, beer]);
+var Plate2 = new Plate("mac and cheese plate", "more cheese", 6, [macNCheese, broccoli, water]);
+var Plate3 = new Plate("hamburger plate", "juicy and nice", 10, [hamburger, fries, sierraMist]);
+
+// var arrOfPlates = [Plate1, Plate2, Plate3];
+
+// var newOrder = new Order(arrOfPlates);
+
+// newOrder.toString();
 
 
 
